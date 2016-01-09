1f96d875076b520e2b9b8ded531c839ecd3a1c73
Jeremy Allston <jicultimate@gmail.com>
2016-01-08 22:02:40 -0500

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gasTrackerApp
+ * @description
+ * # gasTrackerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gasTrackerApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gasTrackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gasTrackerApp
+ */
+angular.module('gasTrackerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
